c3e12f668909cfe6a0f5189a36ae238a80e190b5
Cayke Daniel <contato.cayke@gmail.com>
2023-09-05 11:07:40 -0300

    feat: Pull requests endpoint and messages on

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,25 @@
 import axios from "axios";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
-
-export interface GitHubPayload {
-    action: string;
-    pull_request: {
-        title: string;
-    };
-    sender: {
-        login: string;
-    };
-}
-
-export const sendDiscordMessage = async (message:string) => {
-    if(!discordWebhookUrl) return null;
-    try {
-        await axios.post(discordWebhookUrl, {content: message});
-        console.log("Message send");
-    } catch(error) {
-        //@ts-ignore
-        console.error("Failed to send message: ", error.message);
-    }
+export const sendDiscordMessage = async (message: string) => {
+  if (!discordWebhookUrl) return null;
+  try {
+    await axios.post(discordWebhookUrl, { content: message });
+    console.log("Message send");
+  } catch (error) {
+    //@ts-ignore
+    console.error("Failed to send message: ", error.message);
+  }
 };
 
-export const handleGitHubWebhook = (payload: any) => {
-    
-    if(payload.action === 'opened') {
-        const { title } = payload.pull_request;
-        const senderName = payload.sender.login;
-        const message = `New pull request of ${senderName}: ${title}`;
-        sendDiscordMessage(message);
-    }
-}
+export const handleGitHubWebhookPr = (payload: any) => {
+  const senderName = payload.sender.login;
+  const action = payload.action;
+  const repository = payload.repository.html_url;
+  const repoName = payload.repository.name;
+  const message = `[${senderName}](https://github.com/${senderName}) ${action} a pull-request in [${repoName}](${repository})`;
+  sendDiscordMessage(message);
+};
